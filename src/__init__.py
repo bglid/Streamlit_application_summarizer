acc78cf7cf6fbb5e10cf6671373cab49ac552770
bglid <benjamin.h.glidden@gmail.com>
2024-10-20 14:16:34 -0400

    relative imports issues

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,12 @@
-from .web_scraper import ln_job_scraper
-from .text_processing import keyword_rank, compare_resume
-from .utils import random_user_agent, get_url
+import os
+from . import utils
+from .web_scraper import *
+from .text_processing import *
 
-__all__ = [
-    "ln_job_scraper",
-    "keyword_rank",
-    "compare_resume",
-    "random_user_agent",
-    "get_url"
-]
+# __all__ = [
+#     "ln_job_scraper",
+#     "keyword_rank",
+#     "compare_resume",
+#     "random_user_agent",
+#     "get_url"
+# ]
