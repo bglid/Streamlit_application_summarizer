f21340ff8739b9b848541ba805681404948861d1
bglid <benjamin.h.glidden@gmail.com>
2024-11-01 13:06:28 -0400

    Fixing Error handling for blank vals

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -30,7 +30,11 @@ def main():
     #Getting the URL to the job posting
     job_url = st.text_input('Job Posting url', help='Make sure you link the url to the exact page of the job posting. I.e. not a related link to the posting',
                             key='url')
-    url_input = st.button('Submit Url')    
+    #Setting button to not react until clicked:
+    if 'clicked' not in st.session_state:
+        st.session_state.clicked = False
+    url_input = st.button('Submit Url', on_click=click_button)
+    
 
     # If there is no clear "About the Job" section, post text in manually for time being:
     # job_text = st.text_input('Job Description', help="NOTE: Use this when the job url isn't from LinkedIn, or the posting isn't based on an online source.")
@@ -38,42 +42,52 @@ def main():
     col_1, col_2 = st.columns(2)
 
 
+    #Making sure that a url or resume is posted
+    if url_input and resume == None:
+        st.write('No resume uploaded')
+
 
     #keyword comparison
-    if select == 'Check for Keywords':
-        if url_input: #once the button is clicked on "Submit Url"
-            with st.spinner(text='Processing Text...'):
-                with col_1:
-                    posting_title, posting_time, about_the_job = ln_job_scraper(job_url)
-                    #Getting the text from the "About the job section"
-                    keywords = keyword_rank(about_the_job)
-                    st.subheader('Job Posting Key Phrases:')
-                    st.table(keywords)
-                    st.subheader(posting_title)
-                    st.write(f'Posted {posting_time}')
-            
-                with col_2:
-                    #Getting the text from the resume    
-                    resume_keywords = keyword_rank(resume_text)
-                    st.subheader('Resume Key Phrases:')
-                    st.table(resume_keywords)
+    if url_input and (job_url != ''):
+        if select == 'Check for Keywords':
+            #once the button is clicked on "Submit Url"
+                with st.spinner(text='Processing Text...'):
+                    with col_1:
+                        posting_title, posting_time, about_the_job = ln_job_scraper(job_url)
+                        #Getting the text from the "About the job section"
+                        keywords = keyword_rank(about_the_job)
+                        st.subheader('Job Posting Key Phrases:')
+                        st.table(keywords)
+                        st.subheader(posting_title)
+                        st.write(f'Posted {posting_time}')
+                
+                    with col_2:
+                        #Getting the text from the resume    
+                        resume_keywords = keyword_rank(resume_text)
+                        st.subheader('Resume Key Phrases:')
+                        st.table(resume_keywords)
 
-    #Document similarities
-    if select == 'Compare Resume Similarity':
-        if url_input:
-            with st.spinner(text='Processing Text...', ):
-                posting_title, posting_time, about_the_job = ln_job_scraper(job_url)
-                st.header(posting_title)
-                st.write(f'Posted {posting_time}')
-                with col_1:
-                    similarity_score = compare_resume(resume_text, about_the_job)
+        #Document similarities
+        if select == 'Compare Resume Similarity':
+            if url_input:
+                with st.spinner(text='Processing Text...', ):
+                    posting_title, posting_time, about_the_job = ln_job_scraper(job_url)
+                    st.header(posting_title)
+                    st.write(f'Posted {posting_time}')
+                    with col_1:
+                        similarity_score = compare_resume(resume_text, about_the_job)
+    #Posting a message saying the url is blank
+    if job_url == '':
+        st.write('No job posting url is linked')
                     
     #Creating a button to clear all the data caches
     if st.button("Clear All"):
         st.cache_data.clear()
         # st.session_state['url'] = "" 
 
-        
+#Defining a function to handle the button not reacting until clicked:
+def click_button():
+    st.session_state.clicked = True
                 
                 
 if __name__ == "__main__":
